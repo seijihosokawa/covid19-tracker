4b3ae01dadb26e3ccf9e700111143c7e0f7390ec
seijihosokawa <68927618+seijihosokawa@users.noreply.github.com>
2021-05-16 15:09:20 -0400

    testing commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Covid 19 tracker</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>TEsting change</h2>
     </div>
   );
 }
