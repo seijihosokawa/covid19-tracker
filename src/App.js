7236a362e31feab8479df75ef5a8b983a72b422f
seijihosokawa <68927618+seijihosokawa@users.noreply.github.com>
2021-05-19 01:13:07 -0400

    added linegraph

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ export default function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData}></Table>
           <h3>World Wide new cases</h3>
-          <LineGraph></LineGraph>
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
